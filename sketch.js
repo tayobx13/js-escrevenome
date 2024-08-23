d9c40a9511118ed6b57cf5d4140482bb10e87774
tayobx13 <dzvonek.rafaele@escola.pr.gov.br>
2024-08-23 11:13:46 -0300

    arquivando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+    createCanvas(600, 600);
+  background("white");
+  }
+  function draw() {
+  
+    
+    stroke("red");
+    fill("blue");
+    
+   //console.log(mouseIsPressed)
+    
+    if(mouseIsPressed){
+       rect(mouseX, mouseY, 20, 35);
+  }
+    }
